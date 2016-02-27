8ed1c397d0894cd7ca26f1e49413635fffb250af
Daniel Silva Reis <pigmoth@gmail.com>
2016-02-27 17:14:52 +0000

    teste

---
+++ Aulas/Aula1-UDP/src/Objects/LicencePlate.java
@@ -0,0 +1,15 @@
+package Objects;
+
+public class LicencePlate {
+	String plate_number;
+	String owner_name;
+	
+	
+	public LicencePlate(String plate_number, String owner_name) {
+		super();
+		this.plate_number = plate_number;
+		this.owner_name = owner_name;
+	}
+	
+	
+}
