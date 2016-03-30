828eba6a2c33ed2835216c2dfe006c1dbf824e6e
Daniel Silva Reis <pigmoth@gmail.com>
2016-03-30 14:36:56 +0100

    para bytes

--- SDIS-project/src/test/testSend.java
+++ SDIS-project/src/test/testSend.java
@@ -20,7 +20,7 @@ public class testSend {
 		try {
 			//con.send("PUTCHUNK 1.0 1 b2 8 5 \r\n\r\n abcbdbasdbasdawd");
 			System.out.println("getchunk");
-			con.send("GETCHUNK 1.0 "+peer1.getSenderId()+" 8024415A728490672CFA476712F23FBB3C58167B330C9538B7FB9BF83A50154B"+" 1 \r\n\r\n");
+			con.send(("GETCHUNK 1.0 "+peer1.getSenderId()+" 8024415A728490672CFA476712F23FBB3C58167B330C9538B7FB9BF83A50154B"+" 1 \r\n\r\n").getBytes());
 			
 			/*System.out.println("stored");
 			con.send("STORED 1.0 1 b2 0 \r\n\r\n");
